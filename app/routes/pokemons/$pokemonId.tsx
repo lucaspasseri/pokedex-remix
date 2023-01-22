53b30e072c3570006b7e2331768142b3b337b33f
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2023-01-22 16:45:33 -0300

    feat: add tailwind css

--- app/routes/pokemons/$pokemonId.tsx
+++ app/routes/pokemons/$pokemonId.tsx
@@ -2,12 +2,16 @@ import type { LinksFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 //import { Outlet, Link } from "@remix-run/react";
 import indexStylesUrl from "~/styles/index.css";
+import tailwindStylesUrl from "~/styles/app.css";
 import { db } from "~/utils/db.server";
 import type { LoaderArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
 export const links: LinksFunction = () => {
-	return [{ rel: "stylesheet", href: indexStylesUrl }];
+	return [
+		{ rel: "stylesheet", href: indexStylesUrl },
+		{ rel: "stylesheet", href: tailwindStylesUrl },
+	];
 };
 
 export const loader = async ({ params }: LoaderArgs) => {
@@ -27,10 +31,19 @@ export default function PokemonRoute() {
 	const data = useLoaderData<typeof loader>();
 
 	return (
-		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-			<h1>Welcome to Remix 1</h1>
-			<div>{data.pokemon?.name}</div>
-			<div>{data.pokemon?.number}</div>
+		<div
+			className="p-2 text-3xl font-bold underline bg-slate-50"
+			style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}
+		>
+			<h1 className="p-2 text-3xl font-bold underline bg-slate-50">
+				Welcome to Remix 1
+			</h1>
+			<div className="p-2 text-3xl font-bold underline bg-slate-50">
+				{data.pokemon?.name}
+			</div>
+			<div className="p-2 text-3xl font-bold underline bg-slate-50">
+				{data.pokemon?.number}
+			</div>
 		</div>
 	);
 }
